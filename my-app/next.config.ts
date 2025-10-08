efc3a040ac2b5519f3db53fdb346075d1adeffe0
zartasha <zartashimran12345@gmail.com>
2025-10-08 16:38:17 +0500

    d

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -8,14 +8,38 @@
 
 
 
+// // next.config.js
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: [
+//       "images.unsplash.com",   // Unsplash
+//       "randomuser.me",         // RandomUser avatars
+//       "img.youtube.com"        // ✅ YouTube thumbnails
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "images.unsplash.com",   // Unsplash
-      "randomuser.me",         // RandomUser avatars
-      "img.youtube.com"        // ✅ YouTube thumbnails
+    remotePatterns: [
+      {
+        protocol: 'https',     // or 'http'
+        hostname: 'example.com',
+        port: '',              // optional, leave empty if default
+        pathname: '/**',       // allow all paths
+      },
+      {
+        protocol: 'https',
+        hostname: 'anotherdomain.com',
+        pathname: '/images/**', // allow only images folder
+      },
     ],
   },
 };
